c1cc4f83d2389539ccf6531b07f3cf5f414dac7a
flyingzha <651321049@qq.com>
2018-06-22 10:57:17 +0800

    hello world

---
+++ src/main/java/com/example/demo/controller/DemoController.java
@@ -0,0 +1,19 @@
+package com.example.demo.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by zhangshengle on 2018/6/20.
+ */
+@RestController
+public class DemoController {
+
+    //访问/hello或者/hi任何一个地址，都会返回一样的结果
+    @RequestMapping(value = {"/hello","/hi"},method = RequestMethod.GET)
+    public String hello(){
+        return "hi you!!!";
+    }
+
+}
